951bfc15c9760863cda7a1563f945bace2d7154b
Ubuntu <ubuntu@ip-172-31-94-8.ec2.internal>
2019-05-11 04:02:21 +0000

    Bodega

---
+++ Bodega/sql-scripts/InsertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees (first_name, last_name, department, email) 
+VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active,inventario)
+VALUES ('000000','name',25.00,'shortDescription','longdescription','1',15);
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active,inventario)
+VALUES ('000001','name',25.00,'shortDescription','longdescription','1',15);
+INSERT INTO product (sku,name,price,shortDescription,longDescription,active,inventario)
+VALUES ('000002','name',25.00,'shortDescription','longdescription','1',15);
